45d667b3afb7483d1ea37d51cc9029c690b016bb
madda01 <it20232368@my.sliit.lk>
2021-08-25 16:18:14 +0530

    3rd commit of RM

--- smd-distributors-automated-system/src/main/java/com/smd/servlet/AddFeedbackServlet.java
+++ smd-distributors-automated-system/src/main/java/com/smd/servlet/AddFeedbackServlet.java
@@ -1,6 +1,7 @@
 package com.smd.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -27,17 +28,41 @@ public class AddFeedbackServlet extends HttpServlet {
 				String Rating=request.getParameter("Rating");
 				String Status=request.getParameter("Status");
 
-				IFeedback iFeedbackService = new FeedbackServiceImpl();
-				//to catch the result got from the model class method
-				boolean status=iFeedbackService.addFeedback(Feedback_ID,Cus_ID,Date,Type,Message,Rating,Status);
-				
-				if(status==true) {//if the data was passed to the database successfully 
-					RequestDispatcher dispatcher = request.getRequestDispatcher("success.jsp");
-					dispatcher.forward(request, response);//redirecting from the servlet to
-				}else {//if the data wasn't passed to the database successfully 
-					RequestDispatcher dispatcher = request.getRequestDispatcher("unsuccess.jsp");
-					dispatcher.forward(request, response);//redirecting from the servlet to
+				try{
+					IFeedback iFeedbackService = new FeedbackServiceImpl();
+					//to catch the result got from the model class method
+					boolean status=iFeedbackService.addFeedback(Feedback_ID,Cus_ID,Date,Type,Message,Rating,Status);
+					
+					if(status==true) {//if the data was passed to the database successfully 
+						System.out.println("Successfully Added a book !");
+						
+						PrintWriter out = response.getWriter();
+						out.println("<script src='https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.4/sweetalert2.all.js'></script>");
+						out.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>");
+						out.println("<script>");
+						out.println("$(document).ready(function(){");
+						out.println("swal ( 'Added Successfully' ,  '' ,  'success' );");
+						out.println("});");
+						out.println("</script>"); 
+						
+						RequestDispatcher dispatcher = request.getRequestDispatcher("/RequestC/customerServices.jsp");
+						dispatcher.forward(request, response);//redirecting from the servlet to
+					}else {//if the data wasn't passed to the database successfully 
+						PrintWriter out = response.getWriter();
+						out.println("<script src='https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.4/sweetalert2.all.js'></script>");
+						out.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>");
+						out.println("<script>");
+						out.println("$(document).ready(function(){");
+						out.println("swal ( 'Wrong Entry !' ,  '' ,  'error' );");
+						out.println("});");
+						out.println("</script>"); 
+						RequestDispatcher dispatcher = request.getRequestDispatcher("/RequestC/customerServices.jsp");
+						dispatcher.forward(request, response);//redirecting from the servlet to
+					}
+				}catch (Exception e) {
+					e.printStackTrace();
 				}
+				
 	}
 
 }
