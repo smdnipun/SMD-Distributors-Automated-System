2c174a8e6cfc3c5ed993718aa7dc26d78d38d374
PoornaGTX <onlinepoorna@gmail.com>
2021-09-11 21:24:41 +0530

    cash collector error fix

--- smd-distributors-automated-system/src/main/java/com/smd/service/CreditSalesM.java
+++ smd-distributors-automated-system/src/main/java/com/smd/service/CreditSalesM.java
@@ -161,7 +161,7 @@ public class CreditSalesM {
 			while (rs.next()) {
 				Employee n = new Employee(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
 						rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9),
-						rs.getDouble(10), rs.getInt(11), rs.getString(12));
+						rs.getString(10));
 
 				ll.add(n);
 			}
